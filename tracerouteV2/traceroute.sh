0bcf3fabd3c28f5db060791774bb2f97875321c7
SlaynPool <nicolas.vadkerti@gmail.com>
2018-10-05 20:05:25 +0200

    Bug apparition d'etoile

--- tracerouteV2/traceroute.sh
+++ tracerouteV2/traceroute.sh
@@ -1,27 +1,29 @@
 #!/bin/bash 
-max_TTL=$(traceroute -n -I $1| tail -n 1| cut -c1-2)
-declare -a option=("-T -p 22" "-T -p 53" "-T -p 1" "-T -p 7" "-U -p 7" "-T -p 80")
-id=0
-for t in `seq 1 $max_TTL`
+for i in "$@"
 do
-	for opt in "${!option[@]}"
+	max_TTL=$(traceroute -n -I $i| tail -n 1| cut -c1-2)
+	declare -a option=("-T -p 22" "-T -p 53" "-T -p 1" "-T -p 7" "-U -p 7" "-T -p 80")
+	id=0
+	for t in `seq 1 $max_TTL`
 	do
-		echo $opt
-		if traceroute -n -A -f $t -m $t ${option[opt]} $1| tail -n 1 | cut -d " " -f 3-4| grep  "*"
-		then 
-			echo "option utilisé: ${option[opt]} TTL: $t"
-			if [ $opt -eq 5 ]
-			then
-				
-				echo 'NoID'$id" ""[ASUNKNOWN]""$id" >> route.txt
-				id=$((id+1))
+		for opt in "${!option[@]}"
+		do
+			echo $opt
+			if traceroute -n -A -f $t -m $t ${option[opt]} $i| tail -n 1 | cut -d " " -f 3-4| grep  "*"
+			then 
+				echo "option utilisé: ${option[opt]} TTL: $t"
+				if [ $opt -eq 5 ]
+				then
+					
+					echo 'NoID'$id" ""[ASUNKNOWN]""$id" >> "$i.txt"
+					id=$((id+1))
+					break
+				fi
+			else
+				#traceroute -A -n -f $t -m $t ${option[opt]} $1| tail -n 1 | cut -d " " -f 3-6 >> route.txt
+				traceroute -A -n -f $t -m $t ${option[opt]} $i|tail -n 1 |awk '{print $2" "$3}'>> "$i.txt"
 				break
 			fi
-		else
-			#traceroute -A -n -f $t -m $t ${option[opt]} $1| tail -n 1 | cut -d " " -f 3-6 >> route.txt
-			traceroute -A -n -f $t -m $t ${option[opt]} $1|tail -n 1 |awk '{print $2" "$3}'>> route.txt
-			break
-		fi
+		done
 	done
 done
-
